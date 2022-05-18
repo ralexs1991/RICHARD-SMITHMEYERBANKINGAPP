1a5edd2dc08264de34e811f183671f8a054cf0ed
ralexs1991 <94880853+ralexs1991@users.noreply.github.com>
2022-05-18 17:33:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+const Spa = () => {
+  const [data, setData] = React.useState(null);
+  const [loaded, setLoaded] = React.useState(false);
 
+  React.useEffect(() => {
+      async function getData() {
+          const response = await fetch('./users.json');
+          const json     = await response.json();
+          setData(json);
+          setLoaded(true);
+      }
+      getData();
+  },[])
+
+  return (
+    <HashRouter>
+    {loaded && 
+      <UserContext.Provider value={data.users}>
+        <NavBar/>
+          <div className="container" style={{padding: "20px"}}>
+            <Route path="/" exact component={Home} />
+            <Route path="/createAccount/" component={CreateAccount} />
+            <Route path="/login/" component={Login} />
+            <Route path="/deposit/" component={Deposit} />
+            <Route path="/withdraw/" component={Withdraw} />
+            <Route path="/allData/" component={AllData} />
+          </div>
+        </UserContext.Provider>
+      }
+    </HashRouter>
+  );
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<Spa/>);
