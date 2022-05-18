c80567481db9821b3c820b862091e25b66be2a12
ralexs1991 <94880853+ralexs1991@users.noreply.github.com>
2022-05-18 17:26:31 -0400

    Update context.js

--- context.js
+++ context.js
@@ -1,4 +1,30 @@
 const UserContext = React.createContext(null);
+const Route       = ReactRouterDOM.Route;
+const Link        = ReactRouterDOM.Link;
+const HashRouter  = ReactRouterDOM.HashRouter;
+let ctxUser = [];
+
+function Card(props){
+  const ctx = React.useContext(UserContext);
+    function classes(){
+      const bg  = props.bgcolor ? ' bg-' + props.bgcolor : ' ';
+      const txt = props.txtcolor ? ' text-' + props.txtcolor: ' text-white';
+      return 'card mb-3 ' + bg + txt;
+    }
+  
+    return (
+      <div className={classes()} style={{maxWidth: "18rem"}}>
+        <div className="card-header">{props.header}</div>
+        <div className="card-body">
+          {props.title && (<h5 className="card-title">{props.title}</h5>)}
+          {props.text && (<p className="card-text">{props.text}</p>)}
+          {props.body}
+          {props.status && (<div id='createStatus'>{props.status}</div>)}
+        </div>
+      </div>      
+    );    
+  }
+
 
 function useBankContext() {
   return React.useContext(UserContext);
