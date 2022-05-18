6d569dadfdd91ed8daba3e8590c77b2b668d273d
ralexs1991 <94880853+ralexs1991@users.noreply.github.com>
2022-05-17 22:25:26 -0400

    Add files via upload

---
+++ context.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const UserContext = React.createContext(null);
+
+function useBankContext() {
+  return React.useContext(UserContext);
+}
+
+function ContextProvider({ children }) {
+  const [users, setUsers] = React.useState([
+    {
+      name: 'Kenshin Himura',
+      email: 'rurouni@mit.edu',
+      password: '12345678',
+      balance: 100,
+      loged: false,
+    },
+  ]);
+  function login(user) {
+    const index = users.indexOf(user);
+    const usersCopy = [...users];
+    usersCopy[index].loged = true;
+    setUsers(usersCopy);
+  }
+  function logout(user) {
+    const index = users.indexOf(user);
+    const usersCopy = [...users];
+    usersCopy[index].loged = false;
+    setUsers(usersCopy);
+  }
+  const value = {
+    users,
+    login,
+    logout,
+  };
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+}
+
